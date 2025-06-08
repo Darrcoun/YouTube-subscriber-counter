c625c543fbc9ec0d52e8931b264a903ccab2493c
Darrcoun <coundarren@gmail.com>
2025-06-07 21:31:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,12 @@ def home():
     return "YouTube Subscriber Counter is Running"
 
 @app.route('/smiirl')
+@app.route('/smiirl')
 def smiirl_count():
-    count = get_subscriber_count()
-    return jsonify({"count": count})
+    url = f'https://www.googleapis.com/youtube/v3/channels?part=statistics&id={CHANNEL_ID}&key={YOUTUBE_API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+
+    # Print and return full response for debugging
+    print("DEBUG YouTube API full response:", data)
+    return jsonify(data)
